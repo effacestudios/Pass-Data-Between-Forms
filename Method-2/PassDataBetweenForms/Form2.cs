c81f08f3f6b82770688be49a057a2a3082280dc0
Efface Studios <33323258+effacestudios@users.noreply.github.com>
2019-02-26 03:51:12 +0500

    Released

---
+++ Method-2/PassDataBetweenForms/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PassDataBetweenForms
+{
+    public partial class Form2 : Form
+    {
+        public string Label1
+        {
+            get
+            {
+                return this.label1.Text;
+            }
+            set
+            {
+                this.label1.Text = value;
+            }
+        }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
